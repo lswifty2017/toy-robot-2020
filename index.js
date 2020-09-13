bc2884b5805b5ef09178b17d4aeffba99e3bc231
Liam Swift <lswift@squiz.net>
2020-09-13 15:42:40 +1000

    First revision of index logic - Tested via command line

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('colors');
 const readline = require('readline');
 const parseInput = require('./src/parser/parseInput');
 const rotate = require('./src/actions/rotate');
+const move = require('./src/actions/move');
 
 console.log('Welcome to toy robot.'.green.bold);
 console.log('');
@@ -21,49 +22,105 @@ console.log(
   '- The robot will say the current position and facing direction \n'
 );
 
-const board = Object.freeze({ width: 5, height: 5 });
+const boardDimensions = Object.freeze({ width: 5, height: 5 });
 
 const robotState = {
+  placed: false,
   direction: null,
   positionX: null,
   positionY: null,
 };
 
-// Dev Code
+const handleLineInput = (input) => {
+  const { direction, positionX, positionY, placed } = robotState;
+  const parsedInput = parseInput(input);
 
-const newDirection = rotate('left', 'north');
+  if (!parsedInput.success) {
+    console.log(parsedInput.errorMessage);
+    return rl.prompt();
+  }
 
-console.log(newDirection);
+  const { action } = parsedInput;
 
-//
+  if (action === 'place') {
+    // Check if being placed off the board
+    if (
+      parsedInput.positionX > boardDimensions.width ||
+      parsedInput.positionX < 0 ||
+      parsedInput.positionY > boardDimensions.height ||
+      parsedInput.positionY < 0
+    ) {
+      console.log('Robot has cannot be placed off the board');
+      return rl.prompt();
+    }
 
-// const handleLineInput = (input) => {
-//   const parsedInput = parseInput(input);
+    // Set robot state
+    robotState.direction = parsedInput.direction;
+    robotState.positionX = parsedInput.positionX;
+    robotState.positionY = parsedInput.positionY;
+    robotState.placed = true;
 
-//   if (!parsedInput.success) {
-//     console.log(parsedInput.errorMessage);
-//     rl.prompt();
-//   }
+    return rl.prompt();
+  }
 
-//   if (parsedInput.success) {
-//     console.log(parsedInput.action);
-//     rl.close();
-//   }
+  if (placed) {
+    if (action === 'move') {
+      const newPosition = move({
+        positionX: positionX,
+        positionY: positionY,
+        direction: direction,
+        boardDimensions: boardDimensions,
+      });
 
-//   // console.log(`handling input ${input}`);
-//   // rl.close();
-// };
+      if (newPosition.spaceLimitExceeded) {
+        console.log(
+          'Robot has reached the edge of the board. Please rotate or place robot before moving again'
+        );
+        return rl.prompt();
+      }
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+      robotState.positionX = newPosition.updatedPositionX;
+      robotState.positionY = newPosition.updatedPositionY;
 
-// rl.on('line', handleLineInput)
-//   .on('close', () => {
-//     console.log('Thanks for playing');
-//     process.exit(0);
-//   })
-//   .setPrompt('Robot Command: ');
+      return rl.prompt();
+    }
 
-// rl.prompt();
+    if (action === 'report') {
+      console.log(
+        `My X co-ordinate is ${positionX} and my Y co-ordinate is ${positionY}. I'm facing ${direction}`
+      );
+      return rl.prompt();
+    }
+
+    if (action === 'rotateLeft') {
+      const newDirection = rotate('left', direction);
+      robotState.direction = newDirection;
+      return rl.prompt();
+    }
+
+    if (action === 'rotateRight') {
+      const newDirection = rotate('right', direction);
+      robotState.direction = newDirection;
+      return rl.prompt();
+    }
+  }
+
+  console.log(
+    'Robot has not yet been placed. Please place robot before using any other command'
+  );
+  return rl.prompt();
+};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', handleLineInput)
+  .on('close', () => {
+    console.log('Thanks for playing');
+    process.exit(0);
+  })
+  .setPrompt('Robot Command: ');
+
+rl.prompt();
